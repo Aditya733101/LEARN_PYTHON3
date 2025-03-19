bac7f68299f2c09a1fce9963e7de61b358aa2fcb
Aditya733101 <your-email@example.com>
2025-03-19 12:36:46 +0530

    syntex

---
+++ syntex.py
@@ -0,0 +1,13 @@
+# num = 5;
+num2 = 10;
+# if num > 22:
+#     print("the number is greter then 22")
+# else:
+#     print("the number is less then 22")
+
+
+# The above code is a simple if else statement.
+if num2 > 22:
+    print("the number is greter then 22")
+else:
+    print("the number is less then 22")
