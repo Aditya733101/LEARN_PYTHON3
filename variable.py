294a40f032654fa52c9a01b09a804b2c3e9a5463
Aditya733101 <your-email@example.com>
2025-03-20 11:34:54 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,24 @@
+# Assign Multiple Values
+x, y, z = "orange", "apple", "banna";
+print(x);
+print(y);
+print(z,"\n");
+
+# Unpack a Collection
+fruots =["Aditya","bikram", "sarkar"];
+a,b,c = fruots;
+print(a);       
+print(b);
+print(c);
+# The above code is a simple if else statement.
+# variable print("my name is Aditya sarkar")#print function
+num = "Aditya";
+num2= "Sarkar";
+num3 = "Bikram";
+print(num ,num2 ,num3);
+# another one example
+NumNewVar = "Aditya";
+NumNewVar2 = 20;
+# print(NumNewVar+NumNewVar2);#In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+# To fix this problem, you can use the str() function, which converts the number into a string:
+print(NumNewVar + str(NumNewVar2));
