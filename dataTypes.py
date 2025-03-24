48d55dabf52f6284ff72549ca78fbc3c61eac21d
Aditya733101 <your-email@example.com>
2025-03-24 15:09:36 +0530

    dataTypes in python

---
+++ dataTypes.py
@@ -0,0 +1,14 @@
+#list data types in python
+num = ["Aditya","Sarkar","Bikram"];
+print(num);
+print(num[1]);
+print(type(num));
+#tuple data type in python 
+num2 = ("Adiya", "sarkar","Bikram");
+print(num2);
+print(type(num2));
+# dict  datatype in python
+num3 = {"name":"Aditya","age":20};
+print(num3);
+print(num3["name"]);
+print(type(num3));
