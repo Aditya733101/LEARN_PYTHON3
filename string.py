fc5e0b8311e7bfd37fb375dfa7efe9f2e84de3d0
Aditya733101 <your-email@example.com>
2025-03-26 15:44:33 +0530

    string

--- string.py
+++ string.py
@@ -3,3 +3,18 @@ str = "hello world";
 str2 = 'hello world';
 print(str);
 print(str2);
+# those are symbol of the string are same in python 
+# string concatenation in python
+conCa = "Adutya" + "Sarkar";
+print(conCa);
+# repetition in string 
+repeated = "ADitya "*3;
+print(repeated);
+# accessing string element 
+access = "Aditya";
+print(access[0]);
+print(access[1]);
+# slicing in python 
+silicing = "Aditya"
+print(silicing[0:3]);
+print(silicing[1:5])
