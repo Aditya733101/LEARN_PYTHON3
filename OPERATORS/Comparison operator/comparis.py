e4f6bed5c598a7fd1816b3acb4a96a827884945d
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-03 13:08:25 +0530

    comparison

---
+++ OPERATORS/Comparison operator/comparis.py
@@ -0,0 +1,13 @@
+age = int(input("enter your age - "))
+voting_age =18
+if age >= voting_age:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+
+# ==   Equal to.  x == y
+# !=   Not equal to.  x != y
+# >    Greater than.  x > y
+# <    Less than.  x < y
+# >=   Greater than or equal to.  x >= y
+# <=   Less than or equal to.  x <= y
