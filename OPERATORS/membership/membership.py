f14c92682f97b0747539d3f99f5b7687a6e4d7ba
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-03 13:31:19 +0530

    Add membership operator examples for sequence testing

---
+++ OPERATORS/membership/membership.py
@@ -0,0 +1,9 @@
+#membership operator is used to test if a sequence such as a string or list(in ,not in)
+# in operator
+atr  = [1,2,3,4,5]
+print(3 in atr) # true
+print(6 in atr) # false
+
+# not in operator
+print(3 not in atr) # false
+print(6 not in atr) # true
