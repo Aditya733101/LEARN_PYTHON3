f74b54d2b78ac0594968ce4339c107c703ed3547
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 00:18:28 +0530

    Add example demonstrating the use of istitle() method for title case validation

---
+++ STRING/Cheking method/istitle.py
@@ -0,0 +1,7 @@
+# Returns True if the string first charecter is alphabetic
+text = "Adi"
+if text.istitle():
+    print("string in first charecter is capital ")
+else:
+    print("string in first charecter is not capital")
+    
