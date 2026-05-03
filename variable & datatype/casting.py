617e1e7deb23dea604fa0ed5f50bf1a4d1d609d2
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-03 12:00:47 +0530

    casting

---
+++ variable & datatype/casting.py
@@ -0,0 +1,16 @@
+# casting in python
+#list data types in python
+# 1.int()
+# 2.float()
+# 3.str()
+x = int(1); # x will be 1
+y = int(2.8); # y will be 2
+z = int("3"); # z will be 3
+# then float
+x = float(1); # x will be 1.0
+y = float(2.8); # y will be 2.8
+z = float("3"); # z will be 3.0
+# then str()
+x = str("s1"); # x will be 's1'
+y = str(2); # y will be '2'
+z = str(3.0); # z will be '3.0'
