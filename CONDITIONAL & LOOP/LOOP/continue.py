ccbe5ca213fa60edc2e9414e66fe87afccca8c08
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-03 15:17:57 +0530

    Add example demonstrating the use of continue statement in a loop

---
+++ CONDITIONAL & LOOP/LOOP/continue.py
@@ -0,0 +1,6 @@
+# continue - The continue keyword in Python is a loop control statement used to skip the current iteration and move immediately to the next one.
+for i in range(5):
+    if i == 3:
+        continue  # Skip printing '3'
+    print(i)
+# Output: 0, 1, 2, 4
