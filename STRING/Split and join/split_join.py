727dc74aa0da2f176bdd8862f7e779b1dcec4bf4
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 00:18:32 +0530

    Add example demonstrating the use of split() and join() methods for string manipulation

---
+++ STRING/Split and join/split_join.py
@@ -0,0 +1,16 @@
+# Split a string into a list where each word is a list item:
+
+# split 
+txt = "welcome to the jungle"
+x = txt.split()
+print(x)
+
+
+#  join
+
+names = ["Aditya", "Rahul", "Sara"]
+
+# Using a comma and space as a separator
+result = ", ".join(names)
+print(result) 
+# Output: Aditya, Rahul, Sara
