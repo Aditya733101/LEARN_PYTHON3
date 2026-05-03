46a1d0eefa76672b7b7bd2b1566fde35c8eb0a7c
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 00:18:08 +0530

    Add example demonstrating the use of isalpha() method for alphabetic string validation

---
+++ STRING/Cheking method/isalpha.py
@@ -0,0 +1,8 @@
+# Returns True if all characters in the string are in the alphabet
+
+text = "Adty2a"
+if text.isalpha():
+    print("string is alpha")
+else:
+    print("string is not alpha")
+    
