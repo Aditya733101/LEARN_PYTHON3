e09cdd696a94e6ad69cd80dec5c3baa352564c46
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 00:18:13 +0530

    Add example demonstrating the use of isalnum() method for alphanumeric string validation

---
+++ STRING/Cheking method/alnum.py
@@ -0,0 +1,8 @@
+# return true if string in mixed aplhabet or numerical number 
+text = "Aditya123"
+
+# Check if it is alphanumeric (Letters + Numbers)
+if text.isalnum():
+    print("Is it alphanumeric? Yes")
+else:
+    print("Is it alphanumeric? No")
