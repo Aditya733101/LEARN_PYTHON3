4e88c944527e46453c4294cc178e62ef3f19966a
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 21:34:51 +0530

    Add example demonstrating list operations in Python

---
+++ LIST & TUPLE/list_pyt.py
@@ -0,0 +1,12 @@
+# list of python
+# -> A built-in data type that stores set of values
+# -> it can store elements of differnet type(integer,float,string,etc)
+# ->list mutatble that means list change value after assign 
+# -> list are used to store multiple item in singe variable 
+# example - 1
+# marks = ["12","34","45","67","78"]
+# print(marks) 
+name = ["12", "13", "14"]
+print(name[1])
+name[1] = "30"
+print(name)
