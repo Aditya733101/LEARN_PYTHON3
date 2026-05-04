94782ebcf4a51dbfda2c90defa2acccecef07500
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-04 23:42:31 +0530

    Fix formatting by adding newline at end of file in besic.py

--- introduction/besic.py
+++ introduction/besic.py
@@ -1,3 +1,3 @@
 print("my name is Aditya sarkar")#print
 print("my name is rajib sarkar")#print
-#systex it print and it is used to print the value in the output screen
+#systex it print and it is used to print the value in the output screen 
