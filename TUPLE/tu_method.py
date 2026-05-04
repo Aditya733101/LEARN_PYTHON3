227a24aa134a59f31e222ae3446826a00933223a
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-05 00:25:41 +0530

    Examples for tuple index and count methods

---
+++ TUPLE/tu_method.py
@@ -0,0 +1,6 @@
+#Method -1 :tuple_variable.index(el) return of the index of first occurrence
+tup =(4,2,3,1,1,1)
+print(tup.index(3))
+
+#Method -1 :tuple_variable.count(el) count total occurrences 
+print("total count of occurrence element:- ",tup.count(1))
