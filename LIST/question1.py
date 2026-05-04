8df1061daab8ec8492b0f6fdfbc22b5aa631206b
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-05 00:26:03 +0530

    Display user's favorite movies

---
+++ LIST/question1.py
@@ -0,0 +1,8 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+Movies =[]
+Movies.append(input("Enter the 1st movie name - "))
+Movies.append(input("Enter the 2nd movie name - "))
+Movies.append(input("Enter the 3rd movie name - "))
+print(Movies)
+
+
