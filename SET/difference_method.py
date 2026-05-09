c47289599806c3223e5bd5fc7203bb4472a23fbb
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:31:45 +0530

    example for set difference method

---
+++ SET/difference_method.py
@@ -0,0 +1,7 @@
+# return  the first set uniqe values those value are not seceond set .
+# Return the unique values from the first set that are not in the second set.
+# (-) shortcut of difference set
+set_a ={1,2,3,4}
+set_b ={4,5,6}
+set_c=set_a.difference(set_b)
+print(set_c) 
