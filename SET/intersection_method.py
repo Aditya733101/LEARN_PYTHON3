a2ce5a6a20b7a07a0ab42af137578d2f007a2474
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:31:55 +0530

    example for intersection method in sets

---
+++ SET/intersection_method.py
@@ -0,0 +1,7 @@
+#includes only element present in bo
+# intersection that means common value of two sets 
+set_a ={1,2,3,4}
+set_b ={4,5,6}
+set_c=set_a.intersection(set_b)
+print(set_c)
+# & -> shortcut of intersection set
