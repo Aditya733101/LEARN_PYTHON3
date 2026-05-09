eb2670ec7e7648e36e5bbfeb353551fcb9d5a11b
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:32:29 +0530

    Add example for symmetric_difference method in sets

---
+++ SET/symmetricDiffernce_method.py
@@ -0,0 +1,6 @@
+# The symmetric_difference() method returns all the items present in given sets, except the items in their intersections.
+# (^): shortcut of symmetricdiffernce method
+set_a ={1,2,3,4}
+set_b ={4,5,6}
+set_c=set_a.difference(set_b)
+print(set_c) 
