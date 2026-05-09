df774bd568f9f33874a5cafe3c0f4a50484beba3
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:32:08 +0530

    Add initial set example and characteristics explanation

---
+++ SET/set.py
@@ -0,0 +1,15 @@
+
+# # A set is a collection of unique items. It is used when you don’t want duplicate values.
+# Sets are:
+
+# 1)Unordered (no fixed position/index)
+# 2)Unchangeable items (but you can add/remove items)
+# 3)Do not allow duplicates
+my_set={10,20,30}
+print(my_set)
+
+# Characteristics of Sets:
+# Unordered:Elements have no defined order. You cannot access elements by index.
+# Unique Elements: No duplicates allowed. Each element must be distinct.
+# Mutable: You can add or remove elements after creation.
+# Immutable Elements: individual elements inside a set cannot be modify
