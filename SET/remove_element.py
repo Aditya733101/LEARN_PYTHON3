ffb2710473720971b7ed941ed5ca812bf3486325
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:32:03 +0530

    Add examples for removing items from a set using remove() and discard() methods

---
+++ SET/remove_element.py
@@ -0,0 +1,9 @@
+# remove item in add function
+set_a={1,2,3,4} 
+set_a.remove(3) # showing error while data not found
+print(set_a)
+
+# remove item in add function
+set_a={1,2,3,4} 
+set_a.discard(3) # does not showing  error while data not found
+print(set_a)
