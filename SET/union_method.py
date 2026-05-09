c4b1b944cf84ba42b2995d6547c55e229628a834
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:32:35 +0530

    Add example for union method in sets

---
+++ SET/union_method.py
@@ -0,0 +1,7 @@
+# union() method :- combined two set with unique element
+set_a ={1,2,3,4}
+set_b ={4,5,6}
+set_c=set_a.union(set_b)
+print(set_c)
+# union that means add to set unique value 
+# ( | ) -> short cut of union set method
