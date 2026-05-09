b0e50c2e6b6f43c207fc8aded938d7a355680390
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-09 22:31:21 +0530

    Add example to count students with "A" grade in a tuple

---
+++ LIST/question3.py
@@ -0,0 +1,7 @@
+# Let's Practice
+# WAP to count the number of students with the "A" grade in the following tuple.
+# WAP to program count of 'A' grade student in tuple
+# ["C", "D", "A", "B", "A"]
+student = ["C","D","A","B","A"]
+student.sort()
+print(student)
