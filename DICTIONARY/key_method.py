ffae298792b2a98990eb4e73e6116220a8ed2283
Aditya733101 <bittusarkar733101@gmail.com>
2026-05-10 20:28:40 +0530

    Add example for retrieving keys from a dictionary

---
+++ DICTIONARY/key_method.py
@@ -0,0 +1,12 @@
+student ={
+    "name":"Aditya",
+    "class":9,
+    "subject":{
+        "chem":49,
+        "math":76,
+        "phy":21,
+    }
+}
+print(student)
+print(student.keys())
+# .key() return all key 
